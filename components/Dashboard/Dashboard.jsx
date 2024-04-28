54b1a736be71b638b7ffd920e6cfa1bc7fd583fd
Rahul Arora <rahularora2089@gmail.com>
2024-04-28 17:59:54 +0530

    dashboard component on signin

---
+++ components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="min-h-screen">
+            dashboard
+        </div>
+    );
+}
+
+export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Dashboard from "@/components/Dashboard/Dashboard";
+import Footer from "@/components/Home/Footer";
 import Hero from "@/components/Home/Hero";
 import Navbar from "@/components/Home/Navbar";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
       <Navbar />
-      <Hero />
+      {session ? <Dashboard /> : <Hero />}
+      <Footer />
     </>
   );
 }
