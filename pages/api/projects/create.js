2ea0ad0c05c3db842ece0ca61e1388aaca243134
Rahul Arora <rahularora2089@gmail.com>
2024-04-28 20:42:19 +0530

    create project api

---
+++ pages/api/projects/create.js
@@ -0,0 +1,32 @@
+// create new project
+import connectDb from "@/lib/connectDb";
+import Project from "@/models/Project";
+
+export default async function handler(req, res) {
+  if (!req.method === "POST") {
+    return res
+      .status(405)
+      .json({ success: false, message: "Method not allowed" });
+  }
+
+  const { title, userId } = req.body;
+
+  if (!title || !userId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "title and userId are required" });
+  }
+
+  await connectDb("auth");
+
+  try {
+    const project = new Project({
+      title,
+      userId,
+    });
+    await project.save();
+    res.status(201).json({ success: true, project });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+}
